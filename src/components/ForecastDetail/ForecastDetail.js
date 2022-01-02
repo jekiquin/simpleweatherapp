5e0bea0a25c96fcbd85124104df53095fd3a0bb0
JekIquin <jci.dev.0@gmail.com>
2022-01-02 13:38:18 -0800

    added border color on table

--- src/components/ForecastDetail/ForecastDetail.js
+++ src/components/ForecastDetail/ForecastDetail.js
@@ -2,13 +2,13 @@ import PropTypes from 'prop-types';
 
 export default function ForecastDetail({ forecast }) {
 	return (
-		<div className="grid grid-cols-custom">
-			<p className="text-center border">{forecast.dt}</p>
-			<p className="text-center border">{forecast.main.temp}</p>
-			<p className="text-center border">{forecast.main.temp_min}</p>
-			<p className="text-center border">{forecast.main.temp_max}</p>
-			<p className="text-center border">{forecast.wind.speed}</p>
-			<p className="text-center border">{forecast.weather[0].description}</p>
+		<div className="grid grid-cols-custom odd:bg-gray-200">
+			<p className="table-data">{forecast.dt}</p>
+			<p className="table-data">{forecast.main.temp}</p>
+			<p className="table-data">{forecast.main.temp_min}</p>
+			<p className="table-data">{forecast.main.temp_max}</p>
+			<p className="table-data">{forecast.wind.speed}</p>
+			<p className="table-data">{forecast.weather[0].description}</p>
 		</div>
 	);
 }
--- src/components/TableHeader/TableHeader.js
+++ src/components/TableHeader/TableHeader.js
@@ -6,7 +6,7 @@ export default function TableHeader() {
 	return (
 		<div className="grid grid-cols-custom">
 			{HEADERS.map((header) => (
-				<p className="text-center border bg-gray-400 text-white py-8" key={uuid()}>
+				<p className="table-data bg-gray-400 text-white" key={uuid()}>
 					{header}
 				</p>
 			))}
